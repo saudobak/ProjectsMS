428c5cd6eeae037ff0d175367bafc87c99f2c6e2
saudobak <smith_aurelia@columbusstate.edu>
2025-01-24 19:12:25 -0500

    Committing Tests class

---
+++ src/Tests.java
@@ -0,0 +1,15 @@
+public class Tests {
+
+    //private data fields
+
+//public Constructor(s)
+
+//public set or mutator method for every private data field (except when the data field is read-only)
+
+//public get or accessor method for every private data field (except if it is a secret)
+
+//public toString method that prints out all of the object state (meaning the data in the object instance that you will later instantiate in the main() method)
+
+
+
+}
