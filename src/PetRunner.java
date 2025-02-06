7c7528f79e7eb27f9c7510de75e4a56934aa8e5a
saudobak <smith_aurelia@columbusstate.edu>
2025-02-06 17:02:26 -0500

    Change commit Pet class new data fields

--- src/PetRunner.java
+++ src/PetRunner.java
@@ -3,7 +3,7 @@ public class PetRunner {
     public static void main(String[] args){
         //instantiate Pet
         Pet myPet1 = new Pet();
-        myPet1.setName("Marco");
+        myPet1.setName("Madfgdfgrco");
         Pet myPet2 = new Pet();
         myPet2.setName("Sandy");
         System.out.println(myPet1);
