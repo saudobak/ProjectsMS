2fca44fe9611240082dcac998c50a91c2e6de3ee
saudobak <smith_aurelia@columbusstate.edu>
2025-02-06 17:00:36 -0500

    Second commit Pet class new data fields

--- src/Pet.java
+++ src/Pet.java
@@ -1,37 +1,40 @@
 public class Pet {
     //private data fields
     private String name;
+    private String type;
+    private int age;
 
-    //public Constructors
+    //default constructor
     public Pet() {
         this.setName("");
+        this.type = "";
+        this.age = 0;
+    }
+
+    //custom constructor
+    public Pet(String newName, String newType, int newAge) {
+        this.setName(newName);
+        this.type = newType;
+        this.age = newAge;
     }
 
     //public set or mutator for every private data field (except when the data field is read-only)
-    public void setName(String newName){
+    public void setName(String newName) {
         this.name = newName;
     }
 
     //public get or accessor method for every private data field(except if it is a secret)
-    public String getName(){
+    public String getName() {
         return this.name;
 
     }
+
     //public toString method that prints out all of the objects state
-    public String toString(){
+    public String toString() {
         String output = "Pet name: ";
         output += this.getName();
         return output;
     }
-
-    //main
-    public static void main(String[] args){
-        //instantiate Pet
-        Pet myPet1 = new Pet();
-        myPet1.setName("Marco");
-        Pet myPet2 = new Pet();
-        myPet2.setName("Sandy");
-        System.out.println("Pet information: \n" +  myPet2);
-        System.out.println("Pet information: \n" + "Name: " + myPet1.getName() );
-    }
 }
+
+
---
+++ src/PetRunner.java
@@ -0,0 +1,13 @@
+public class PetRunner {
+    //main
+    public static void main(String[] args){
+        //instantiate Pet
+        Pet myPet1 = new Pet();
+        myPet1.setName("Marco");
+        Pet myPet2 = new Pet();
+        myPet2.setName("Sandy");
+        System.out.println(myPet1);
+        System.out.println(myPet2);
+    }
+}
+
